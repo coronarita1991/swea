7335512d48e900792145180e31e57e464e96fe92
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2025-03-14 12:35:09 +0900

    [level 1] Title: 신고 결과 받기, Time: 1452.23 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,34 +1,12 @@
-from collections import defaultdict
 def solution(id_list, report, k):
-    
-    id_dict:dict[(str, int)] = defaultdict()
-    reported_dict:dict[(str, int)] = defaultdict()
-    report_list_dict:dict[(str, set)] = defaultdict(set)
-    
-    for id in id_list:
-        id_dict[id] = 0
-        reported_dict[id] = 0
-        
-    for s in report:
-        id, reported_id = s.split()
-        if reported_id not in report_list_dict[id]:
-            report_list_dict[id].add(reported_id)
-            reported_dict[reported_id] += 1
-        
-    # k회 이상
-    # print(report_list_dict, reported_dict)
-    rep_list = []
-    for reported_id, val in reported_dict.items():
-        if val >= k:
-            rep_list.append(reported_id)
-    
-    for id, indiv_list in report_list_dict.items():
-        for user in rep_list:
-            if user in indiv_list:
-                id_dict[id]+=1
-    
-    result = []
-    for id in id_list:
-        result.append(id_dict[id])
-    
-    return result
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
