f2201b66c0dd6ceb5e86deaf757c3d76e5e092ce
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2025-03-14 12:01:25 +0900

    [level 3] Title: 표 편집, Time: 251.40 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.py
+++ 프로그래머스/3/81303. 표 편집/표 편집.py
@@ -1,38 +1,32 @@
 def solution(n, k, cmd):
-    # 삭제된 인덱스 저장
-    deleted = [] 
+    deleted = []
     
-    # 링크드리스트 인덱스 저장
     up = [i-1 for i in range(n+2)]
-    down = [i+1 for i in range(n+1)]
+    down = [i+1 for i in range(n+2)]
     
-    # 현재 위치
-    k += 1
+    k+=1
     
-    # cmd 처리
     for cmd_i in cmd:
-        # 명령어 처리 : 삭제
-        if cmd_i.startswith("C"):
-            deleted.append(k)
+        if cmd_i.startswith('C'):
+            # linked list
             up[down[k]] = up[k]
             down[up[k]] = down[k]
-            k = up[k] if n < down[k] else down[k]
-        # 명령어 처리 : 복원
-        elif cmd_i.startswith("Z"):
-            restore = deleted.pop()
-            down[up[restore]] = restore
+            deleted.append(k)
+            k = down[k] if down[k] <= n else up[k]
+                 
+        elif cmd_i.startswith('Z'):
+            restore = deleted.pop()     
             up[down[restore]] = restore
-        # 현재 위치 이동
-        else :
-            action, num = cmd_i.split()
-            if action == 'U': 
-                for _ in range(int(num)):
-                    k = up[k]
-            else : 
-                for _ in range(int(num)):
-                    k = down[k]
+            down[up[restore]] = restore
+                 
+        else:
+            op, num = cmd_i.split()
+            for _ in range(int(num)):
+                k = up[k] if op == 'U' else down[k]
                     
-    answer = ["O"] * n
-    for i in deleted:
-        answer[i-1] = "X"
-    return "".join(answer)
+                    
+    answer = ['O'] * n
+    for i in deleted :
+        answer[i-1] = 'X'
+    return ''.join(answer)
+        
