5eb79f39d9436cc4c73a7693112f3e81de63ae36
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2025-03-14 12:41:17 +0900

    [level 1] Title: 신고 결과 받기, Time: 193.53 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,12 +1,28 @@
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)    
-    reports = {x : 0 for x in id_list}
-
+    
+    reported_user = {}
+    count = {}
+    
     for r in set(report):
-        reports[r.split()[1]] += 1
-
-    for r in set(report):
-        if reports[r.split()[1]] >= k:
-            answer[id_list.index(r.split()[0])] += 1
-
+        user_id, reported_id = r.split()
+        if reported_id not in reported_user :
+            reported_user[reported_id] = set()
+        reported_user[reported_id].add(user_id)
+        
+    for reported_id, user_id_lst in reported_user.items():
+        if len(user_id_lst) >= k :
+            for uid in user_id_lst:
+                if uid not in count:
+                    count[uid] = 1
+                else:
+                    count[uid] +=1
+                    
+    
+    answer = []
+    for i in range(len(id_list)):
+        if id_list[i] not in count:
+            answer.append(0)
+        else:
+            answer.append(count[id_list[i]])
+    
     return answer
