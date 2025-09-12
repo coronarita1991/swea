ee4c6f40a5040b0a2b39399a556556e721c8bda3
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2025-09-12 22:13:10 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.05 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,33 +1,9 @@
 def solution(s):
-    answer = ''
-    temp = []
-    def transform_word(word):
-        nw = ''
-        for i, ch in enumerate(word):
-            if i % 2 == 0:
-                nw += ch.upper()
-            else : 
-                nw += ch.lower()
-        return nw
-             
-    # 단어 추출
-    words = s.split()
-    for word in words:
-        temp.append(transform_word(word))
-
-    import re
-    
-    # 임시로 _ 치환
-    s = s.replace(' ', '_')
-    s = re.sub(r'[A-Za-z]+', '^', s)
-    
-    for ch in s :
-        if ch == '^': 
-            answer+=temp.pop(0)
-        else:   
-            answer += ' '
-    
-    
-    
-    
-    return answer
+    result = []
+    for word in s.split(' '):
+        new_word = ''
+        for idx,w in enumerate(word):
+            if idx%2 ==0 : new_word += w.upper()
+            else : new_word += w.lower()
+        result.append(new_word)
+    return ' '.join(result)
