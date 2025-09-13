a570b48fda50578c8e720b3ffea9f676398b9746
Junghoon Lee <169136233+coronarita1991@users.noreply.github.com>
2025-09-13 20:13:58 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/README.md
+++ 프로그래머스/1/159994. 카드 뭉치/README.md
@@ -1,10 +1,10 @@
 # [level 1] 카드 뭉치 - 159994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159994) 
 
 ### 성능 요약
 
-메모리: 9.34 MB, 시간: 0.01 ms
+메모리: 9.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 14일 00:33:07
+2025년 09월 13일 20:13:55
 
 ### 문제 설명
 
--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,20 +1,14 @@
-from collections import deque
-
 def solution(cards1, cards2, goal):
-
-    cards1 = deque(cards1)
-    cards2 = deque(cards2)
-    goal = deque(goal)
-    
-    while goal :
-        if cards1 and cards1[0] == goal[0]:
-            cards1.popleft()
-            goal.popleft()
-        elif cards2 and cards2[0] == goal[0]:
-            cards2.popleft()
-            goal.popleft()
-            
-        else:
-            break
+    answer = ''
+    # 순서가 중요하다. 
+    p1, p2 = 0, 0
+    for word in goal :
+        # p1, p2 둘 중에서 가능하면 포인터 갱신
+        if p1 < len(cards1) and cards1[p1] == word : 
+            p1 += 1
+        elif p2 < len(cards2) and cards2[p2] == word :
+            p2 += 1
         
-    return "Yes" if not goal else "No"
+        else : 
+            return "No"
+    return "Yes"
